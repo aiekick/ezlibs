35ec1baa7743d66241966f56fd133c1c335f2b72
aiekick <aiekick@funparadigm.com>
2026-06-22 18:46:32 +0200

    -

--- include/ezlibs.hpp
+++ include/ezlibs.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
 #define ezLibs_Label "ezLibs"
-#define ezLibs_BuildNumber 57
+#define ezLibs_BuildNumber 58
 #define ezLibs_MinorNumber 0
 #define ezLibs_MajorNumber 0
-#define ezLibs_BuildId "0.0.57"
-#define ezLibs_BuildIdNum 000057
+#define ezLibs_BuildId "0.0.58"
+#define ezLibs_BuildIdNum 000058
 #define ezLibs_FigFontLabel u8R"(            _       _  _                    ___       ___  
            | |     (_)| |                  / _ \     / _ \ 
   ___  ____| |      _ | |__   ___  __   __| | | |   | | | |
